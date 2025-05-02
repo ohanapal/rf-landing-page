81eb71d3d58133a31c863792ff9aa4cffcd071a4
Naimur <naeemhasan28@gmail.com>
2025-05-02 18:35:25 +0600

    Update package dependencies, add @radix-ui/react-slot for improved component functionality, and enhance Home component layout with new styling and image integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,41 @@
+import dashboardImg from '@/assets/images/homepage/hero/hero.jpg'
 import { BackgroundBeamsWithCollision } from '@/components/ui/background-beams-with-collision'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800'>
       <BackgroundBeamsWithCollision>
-        <h1>hello</h1>
+        <div className='flex flex-col items-center justify-center h-full max-w-3xl mx-auto text-center text-balance gap-8'>
+          <h1 className='text-neutral-100 text-7xl font-semibold'>
+            Deploy your website in seconds, not hours.
+          </h1>
+          <p className='text-neutral-100 font-medium'>
+            With our state of the art, cutting edge, we are so back kinda
+            hosting services, you can deploy your website in seconds.
+          </p>
+          <div className='flex items-center justify-center gap-5'>
+            <Button>Get Started</Button>
+            <Button variant='outline'>Learn More</Button>
+          </div>
+        </div>
       </BackgroundBeamsWithCollision>
-      <div className='flex items-center justify-center'></div>
+      <div className='max-w-7xl mx-auto rounded-4xl border-2 p-2 bg-gray-400'>
+        <div className='w-full h-full bg-gray-500 rounded-3xl p-2'>
+          <div className='w-full h-full bg-gray-600 rounded-2xl p-2'>
+            <div className='w-full h-full bg-gray-700 rounded-xl p-2'>
+              <div className='w-full h-full bg-gray-800 rounded-lg p-2'>
+                <Image
+                  src={dashboardImg}
+                  alt='dashboard'
+                  className='w-full h-full object-cover rounded-md'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
