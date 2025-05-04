9f2dc1588c21493d6455e4059dcd5d273f03c778
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-05-04 12:06:26 +0600

    Implement scroll-based Navbar styling to enhance user experience. Add state management for scroll detection and adjust Navbar appearance based on scroll position for improved visual feedback.

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,13 +1,45 @@
+'use client'
+
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 import logoDark from '@/assets/logo/logo-dark.png'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import { Button } from '../ui/button'
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY
+      console.log(scrollTop)
+      setIsScrolled(scrollTop > 100)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <nav className='fixed top-0 left-0 right-0 z-50 w-full h-16 backdrop-blur-lg'>
-      <div className='container mx-auto flex items-center justify-between h-full py-2'>
+    <nav
+      className={cn('fixed top-0 left-0 right-0 z-50 h-16 transition-all duration-300', {
+        'top-5': isScrolled,
+      })}
+    >
+      <div
+        className={cn(
+          'container mx-auto flex items-center justify-between h-full py-2 w-full transition-all duration-300 backdrop-blur-lg rounded-none px-0',
+          {
+            'bg-neutral-300/20 max-w-3xl rounded-full px-5': isScrolled,
+          },
+        )}
+      >
         <Link href='/' className='h-full'>
           <Image src={logoDark} alt='logo' className='w-auto h-full' />
         </Link>
