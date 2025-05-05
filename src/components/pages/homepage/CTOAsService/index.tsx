5aca1d7d5e5b32723217dbf9d73327bc1fba1298
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-05-05 16:51:43 +0600

    Update lucide-react dependency and enhance CTA component styling
    - Updated lucide-react package to version 0.507.0 for improved features and performance.
    - Added GlowingEffect component to the CTA section for enhanced visual appeal.
    - Refactored layout of the CTA component to improve content presentation and user engagement.

--- src/components/pages/homepage/CTOAsService/index.tsx
+++ src/components/pages/homepage/CTOAsService/index.tsx
@@ -1,3 +1,4 @@
+import { GlowingEffect } from '@/components/ui/glowing-effect'
 import data from '@/data/homepage.json'
 import { CircleCheck } from 'lucide-react'
 import Image from 'next/image'
@@ -27,30 +28,40 @@ export default function CTOAsService() {
         </div>
 
         <div className='rounded-md overflow-hidden border border-white/5 bg-[#0d0d0d]'>
-          <Image
-            src={ctoAsService.rightContent.imageUrl}
-            alt={ctoAsService.rightContent.title}
-            width={600}
-            height={200}
-            className='w-full h-auto aspect-[3/1] object-cover'
-          />
+          <div className='relative h-full border rounded-md'>
+            <GlowingEffect
+              blur={0}
+              borderWidth={3}
+              spread={80}
+              glow={true}
+              disabled={false}
+              proximity={64}
+              inactiveZone={0.01}
+            />
+            <Image
+              src={ctoAsService.rightContent.imageUrl}
+              alt={ctoAsService.rightContent.title}
+              width={600}
+              height={200}
+              className='w-full h-auto aspect-[3/1] object-cover'
+            />
+            <div className='p-6 space-y-5'>
+              <h3 className='text-2xl font-bold'>{ctoAsService.rightContent.title}</h3>
+              <div className='space-y-3'>
+                {ctoAsService.rightContent.items.map((item) => (
+                  <div key={item.id} className='flex gap-x-3'>
+                    <CircleCheck className='size-6 text-primary mt-1' />
+                    <p className='text-base'>
+                      <span className='font-bold'>{item.title}</span> {item.description}
+                    </p>
+                  </div>
+                ))}
+              </div>
 
-          <div className='p-6 space-y-5'>
-            <h3 className='text-2xl font-bold'>{ctoAsService.rightContent.title}</h3>
-            <div className='space-y-3'>
-              {ctoAsService.rightContent.items.map((item) => (
-                <div key={item.id} className='flex gap-x-3'>
-                  <CircleCheck className='size-6 text-primary mt-1' />
-                  <p className='text-base'>
-                    <span className='font-bold'>{item.title}</span> {item.description}
-                  </p>
-                </div>
-              ))}
-            </div>
-
-            <div className='bg-[#121924] rounded-md p-5 space-y-2.5'>
-              <p className='text-muted-foreground text-sm'>{ctoAsService.rightContent.quote.text}</p>
-              <p className='text-sm font-bold'> — {ctoAsService.rightContent.quote.author}</p>
+              <div className='bg-[#121924] rounded-md p-5 space-y-2.5'>
+                <p className='text-muted-foreground text-sm'>{ctoAsService.rightContent.quote.text}</p>
+                <p className='text-sm font-bold'> — {ctoAsService.rightContent.quote.author}</p>
+              </div>
             </div>
           </div>
         </div>
