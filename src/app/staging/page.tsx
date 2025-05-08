5583a688e6aef0fee5010415413ffa5c0b505113
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-05-08 14:32:31 +0600

    Update ProjectPage and ProjectTimeline for enhanced user experience
    - Improved layout and spacing in ProjectPage and ProjectTimeline components for better visual flow.
    - Enhanced image presentation in the gallery for improved responsiveness across devices.
    - Refined timeline layout to support diverse content sizes while maintaining a clean aesthetic.

---
+++ src/app/staging/page.tsx
@@ -0,0 +1,31 @@
+import About from '@/components/pages/homepage/About'
+import Contact from '@/components/pages/homepage/Contact'
+import CTA from '@/components/pages/homepage/CTA'
+import CTOAsService from '@/components/pages/homepage/CTOAsService'
+import Hero from '@/components/pages/homepage/Hero'
+import Projects from '@/components/pages/homepage/Projects'
+import RebelAdvantage from '@/components/pages/homepage/RebelAdvantage'
+import Services from '@/components/pages/homepage/Services'
+import Team from '@/components/pages/homepage/Team'
+import Testimonials from '@/components/pages/homepage/Testimonials'
+import data from '@/data/homepage.json'
+
+export default function StagingPage() {
+  const { cta, cta2, cta3, hero } = data
+  return (
+    <main>
+      <Hero hero={hero} />
+      <About />
+      <Services />
+      <CTA cta={cta} />
+      <Projects />
+      <RebelAdvantage />
+      <CTOAsService />
+      <CTA cta={cta2} />
+      <Team />
+      <Testimonials />
+      <Contact />
+      <CTA cta={cta3} />
+    </main>
+  )
+}
