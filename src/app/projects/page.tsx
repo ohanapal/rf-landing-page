3e4f3819af238a8e2e9d9c50885a27d3f07dfe9b
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-05-08 17:22:21 +0600

    Update Projects component to include filtering for pinned projects and enhance project data structure
    - Implemented filtering logic in Projects component to display only pinned projects.
    - Updated IProject interface to include isPinned property for better project management.
    - Enhanced projects.json to reflect the new isPinned status for relevant entries.

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,22 @@
+import Heading, { HeadingTitle } from '@/components/pages/homepage/common/Heading'
+import ProjectCard from '@/components/pages/homepage/Projects/ProjectCard'
+import data from '@/data/homepage.json'
+import projectsData from '@/data/projects.json'
+
+export default function ProjectsPage() {
+  const { projects } = data
+  const { projects: allProjects } = projectsData
+  return (
+    <section className='py-12 md:py-20'>
+      <div className='container'>
+        <Heading title={projects.title as HeadingTitle[]} description={projects.description} />
+      </div>
+
+      <div className='grid grid-cols-1 min-[450px]:grid-cols-2 md:grid-cols-3 gap-3 sm:gap-4 md:gap-5 max-w-4xl mx-auto px-4'>
+        {allProjects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </section>
+  )
+}
