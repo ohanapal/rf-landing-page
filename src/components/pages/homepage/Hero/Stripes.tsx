af6793114779b798cede89ec8961137bb0659bb1
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-05-12 14:15:25 +0600

    Update Stripes component styles for enhanced visual consistency
    - Increased opacity of the stripes for better visibility.
    - Changed background colors from neutral to slate shades for a modern look.
    - Adjusted border colors to match the new slate theme, improving overall aesthetics.

--- src/components/pages/homepage/Hero/Stripes.tsx
+++ src/components/pages/homepage/Hero/Stripes.tsx
@@ -1,25 +1,25 @@
 export default function Stripes() {
   return (
     <div
-      className='absolute inset-0 flex -rotate-45 isolate h-full w-full scale-200 translate-x-40 opacity-25'
+      className='absolute inset-0 flex -rotate-45 isolate h-full w-full scale-200 translate-x-40 opacity-30'
       style={{
         WebkitMaskImage: 'linear-gradient(135deg, transparent 0%, black 30%, black 100%)',
         maskImage: 'linear-gradient(135deg, transparent 0%, black 30%, black 100%)',
       }}
     >
-      <div className='w-1/10 bg-neutral-700 h-full'></div>
-      <div className='w-[3%] bg-neutral-800 h-full border-r border-l border-neutral-400 border-dashed'></div>
-      <div className='w-1/10 bg-neutral-700 h-full'></div>
-      <div className='w-[3%] bg-neutral-800 h-full border-r border-l border-neutral-400 border-dashed'></div>
-      <div className='w-1/10 bg-neutral-700 h-full'></div>
-      <div className='w-[3%] bg-neutral-800 h-full border-r border-l border-neutral-400 border-dashed'></div>
-      <div className='w-1/10 bg-gradient-to-b from-neutral-800 to-neutral-400 h-full'></div>
-      <div className='w-[3%] bg-neutral-800 h-full border-r border-l border-neutral-400 border-dashed'></div>
-      <div className='w-1/10 bg-neutral-800 h-full'></div>
-      <div className='w-[3%] bg-neutral-800 h-full border-r border-l border-neutral-400 border-dashed'></div>
-      <div className='w-1/10 bg-neutral-700 h-full'></div>
-      <div className='w-[3%] bg-neutral-800 h-full border-r border-l border-neutral-400 border-dashed'></div>
-      <div className='w-1/10 bg-neutral-700 h-full'></div>
+      <div className='w-1/10 bg-slate-800 h-full'></div>
+      <div className='w-[3%] bg-slate-900 h-full border-r border-l border-slate-400 border-dashed'></div>
+      <div className='w-1/10 bg-slate-800 h-full'></div>
+      <div className='w-[3%] bg-slate-900 h-full border-r border-l border-slate-400 border-dashed'></div>
+      <div className='w-1/10 bg-slate-800 h-full'></div>
+      <div className='w-[3%] bg-slate-900 h-full border-r border-l border-slate-400 border-dashed'></div>
+      <div className='w-1/10 bg-gradient-to-b from-slate-900 to-slate-400 h-full'></div>
+      <div className='w-[3%] bg-slate-900 h-full border-r border-l border-slate-400 border-dashed'></div>
+      <div className='w-1/10 bg-slate-900 h-full'></div>
+      <div className='w-[3%] bg-slate-900 h-full border-r border-l border-slate-400 border-dashed'></div>
+      <div className='w-1/10 bg-slate-800 h-full'></div>
+      <div className='w-[3%] bg-slate-900 h-full border-r border-l border-slate-400 border-dashed'></div>
+      <div className='w-1/10 bg-slate-800 h-full'></div>
     </div>
   )
 }
